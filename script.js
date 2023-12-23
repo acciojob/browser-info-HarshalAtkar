3a07c27bdcf03598259d95e644945333808c6837
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2023-12-23 14:45:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+let agent=navigator.userAgent;
+let agent1=navigator.appName;
+let agent2=navigator.appVersion;
+getElementbyID("you are using"   agent,"version"  agent1,"version" agent2);
