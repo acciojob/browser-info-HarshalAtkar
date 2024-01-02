35e1a4bc4ce93e68f8ac49df65413289946f6f4d
Harshal Atkar <142751341+HarshalAtkar@users.noreply.github.com>
2024-01-02 21:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//your JS code here. If required.
-let agent=navigator.userAgent;
-let agent1=navigator.appName;
-let agent2=navigator.appVersion;
-let div=getelementbyId("browser-info")
-	div.innertext("you are using"   ${agent},"version" ${ agent1},"version" ${agent2});
+let agent = navigator.userAgent;
+  let agent1 = navigator.appName;
+  let agent2 = navigator.appVersion;
+  let div = document.getElementById("browser-info");
+  div.innerText =( "You are using " + agent + ", version " + agent1 + ", version " + agent2);
